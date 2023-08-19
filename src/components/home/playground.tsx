391f044a8d7f133ae4a558780811b41e2835195d
koutaro-masaki <koutaro.masaki@glucose.jp>
2023-08-20 04:16:38 +0900

    feat: PlaygroundのURLのセーフティネット

--- src/components/home/playground.tsx
+++ src/components/home/playground.tsx
@@ -4,5 +4,13 @@ type Props = {
   src: string
 }
 export const Playground: React.FC<Props> = ({ src }) => {
-  return <iframe src={src} allow="clipboard-read; clipboard-write" width="100%" height="100%" allowFullScreen />
+  return (
+    <iframe
+      src={src.replace('play?#code', 'play#code')}
+      allow="clipboard-read; clipboard-write"
+      width="100%"
+      height="100%"
+      allowFullScreen
+    />
+  )
 }
